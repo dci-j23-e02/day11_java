d39f6f236d15847cfee2110614b2770b8befb9c6
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-19 10:56:32 +0200

    init  commit

---
+++ loops/main/src/WhileLoop.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+
+public class WhileLoop {
+
+
+  public static void main(String[] args) {
+    int i = 0;
+
+    while (i < 5){ /* break condition 5 < 5 : false : break the loop */
+      System.out.println(i); // 0 , 1 , 2 , 3 , 4
+      i++; // i = i +1; // i : 1 , 2 , 3 , 4 , 5
+    }
+
+    // System.out.println(i);
+
+  }
+}
